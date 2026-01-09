6e850bc0dcd42fd05d06228fb413a1b9d821c85a
AlexPonomarev1992 <sanadogggg@gmail.com>
2026-01-09 20:20:05 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ДОБАВЬТЕ ЭТУ СТРОКУ
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
